f8b6a47c5325f3a7483cbec614b745e26f242eef
woo00oo <96woo94@naver.com>
2021-05-26 11:05:23 +0900

    h2 데이터베이스 설치, 순수 JDBC 코드 작성

--- src/main/java/hello/springintro/SpringConfig.java
+++ src/main/java/hello/springintro/SpringConfig.java
@@ -1,22 +1,30 @@
 package hello.springintro;
 
+import hello.springintro.repository.JdbcMemberRepository;
 import hello.springintro.repository.MemberRepository;
 import hello.springintro.repository.MemoryMemberRepository;
 import hello.springintro.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-//    }
+        return new JdbcMemberRepository(dataSource);
+    }
 
 }
